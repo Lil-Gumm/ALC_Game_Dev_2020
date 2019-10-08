08e05dc9d11cacd4ecb2496355b9086503ae0359
Ethan Manuta <ethan.m264@stu.nebo.edu>
2019-10-08 10:29:22 -0600

    did it

---
+++ CreateWithCode/Prototype 2/Prototype 2/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+
+{
+    public float horizontalInput;
+    public float speed = 15f;
+    public float xrange = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xrange)
+        {
+            transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xrange)
+        {
+            transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+}
