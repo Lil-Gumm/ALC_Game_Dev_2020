1ba8053b6f94305c592151c1b91145b6a2b43f33
Ethan Manuta <ethan.m264@stu.nebo.edu>
2019-10-10 10:28:05 -0600

    gg

--- CreateWithCode/Prototype 2/Prototype 2/Assets/Scripts/PlayerController2.cs
+++ CreateWithCode/Prototype 2/Prototype 2/Assets/Scripts/PlayerController2.cs
@@ -8,6 +8,8 @@ public class PlayerController2 : MonoBehaviour
     public float horizontalInput;
     public float speed = 15f;
     public float xrange = 15f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +28,13 @@ public class PlayerController2 : MonoBehaviour
         {
             transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Horizontal");
 
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
