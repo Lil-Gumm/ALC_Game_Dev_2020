e0f4a404af56409caef3c51a9bf191748ac3b815
Ethan Manuta <ethan.m264@stu.nebo.edu>
2019-10-16 10:27:51 -0600

    a

--- CreateWithCode/Prototype 2/Prototype 2/Assets/Scripts/PlayerController2.cs
+++ CreateWithCode/Prototype 2/Prototype 2/Assets/Scripts/PlayerController2.cs
@@ -32,7 +32,9 @@ public class PlayerController2 : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0
+
+            ))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
